5f784b5e2ad6ed454f80b35ad574f046a990803c
John Newcombe (GlassTTY) <jnewcombeuk@gmail.com>
2025-03-26 22:30:50 +0000

    Added styles and fixed Mode7 font.

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using System.Linq;
 using System.Net.Mime;
+using System.Runtime.CompilerServices;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
+using Avalonia.Layout;
 using Avalonia.Media;
+using Avalonia.Media.Fonts;
 using Tmds.DBus.Protocol;
 using Bitmap = Avalonia.Media.Imaging.Bitmap;
 using Brushes = Avalonia.Media.Brushes;
@@ -23,55 +28,22 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
 
-        this.ExtendClientAreaToDecorationsHint = true;
+        //this.ExtendClientAreaToDecorationsHint = true;
 
         var thicknessZero = Thickness.Parse("0");
-        display.Margin = Thickness.Parse("10");
-        display.FirstColumn = 0;
+        //display.Margin = Thickness.Parse("10");
+        //display.FirstColumn = 0;
 
 
-        // Uniform grid
         // Loop through attaching font characters from the
         // display data array i.e. (960 bytes of virtual screen memory)
-        for (var i = 0; i < 24 * 40; i++)
+        // Last row is blank.
+        for (var i = 0; i < (24 * 40); i++)
         {
-            //var panel = new Panel();
-            //panel.Margin = thicknessZero;
-
-
-            var background = new Rectangle
-            {
-                Margin = thicknessZero,
-                Fill = Brushes.Yellow
-            };
-
             var g = GetSixelGraphic(0);
-
-            //var foreground = new Image()
-            //{
-            //  Margin = thicknessZero,
-            //    Height = background.Height,
-            //    Width = background.Width,
-
-            //    // just adding a single character for now
-            //    Source= new Bitmap("/Users/john/Library/CloudStorage/Dropbox/Repositories/telstar-client-avalonia/telstar-client-avalonia/Assets/bitmap_40.png"),
-            //Source= new Bitmap("/Users/john/Library/CloudStorage/Dropbox/Repositories/telstar-client-avalonia/telstar-client-avalonia/Assets/untitled.png"),
-
-            //panel.Children.Add(background);
-            //panel.Children.Add(foreground);
-            //var rune = new PathIcon
-            //{
-            //    Foreground = Brushes.Yellow,
-            //    Background = Brushes.Blue,
-            //    Margin = thicknessZero,
-            //    Padding =  thicknessZero,
-            //    FontStretch = FontStretch.UltraExpanded,
-            //    BackgroundSizing = BackgroundSizing.OuterBorderEdge,
-            //    BorderThickness = thicknessZero,
-            //};
-
+            
+            // Add to Uniform grid 40 x 24
             display.Children.Add(g);
-            var panel = display.Children[i];
         }
     }
 
@@ -85,47 +57,34 @@ public partial class MainWindow : Window
         Debug.WriteLine("Menu Click!");
     }
 
-    private static UniformGrid GetSixelGraphic(int charNumber)
+    private static Label GetSixelGraphic(int charNumber)
     {
         var thicknessZero = Thickness.Parse("0");
 
+        /* Mode 7 font
+         * Graphics start at e201
+         * Non-contiguous graphics start at e2c1
+         * Upper part of Alpha double height e021
+         * Lower part of Alpha double height e121
+         *
+         */
+        
         // create a sixel
-        var sixel = new UniformGrid()
+        var label = new Label()
         {
-            Columns = 2,
-            Rows = 3,
-            Background = Brushes.Blue // blue will highlight any width and height issues.
-        };
-
-        // what about iterating cols and rows... etc
-        for (var i = 0; i < (2 * 3); i++)
-        {
-            // create a background pixel
-            var pixel = new Rectangle
-            {
-                Margin = thicknessZero,
-                Fill = Brushes.Black,
-            };
-
-            // create a foreground pixel where appropriate
-
-            if (i == 0 || i == 1 || i == 4 || i == 5)
-            {
-                pixel.Fill = Brushes.Yellow;
-            }
-
-            sixel.Children.Add(pixel);
-        }
-
-
-        var cell = sixel.Children[0];
-
-        var background = new Rectangle
-        {
+            FontSize = 22,
+            Background = Brushes.Black,
+            Foreground = Brushes.White,
+            Content = "\xe276",
+            Padding = thicknessZero,
             Margin = thicknessZero,
-            Fill = Brushes.Yellow
+            FontStretch = FontStretch.UltraExpanded
+            
         };
 
-        return sixel;
+        // set the style i.e. Mode 7 font.
+        label.Classes.Add("mode7");
+
+        return label;
     }
 }
