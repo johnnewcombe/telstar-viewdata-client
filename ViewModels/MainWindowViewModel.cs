98e615dc54678f5e2d2c07e0f3b00fd2586e5170
John Newcombe (GlassTTY) <jnewcombeuk@gmail.com>
2025-03-29 15:25:19 +0000

    Added Asynchronous client.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,37 +1,67 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing.Printing;
 using System.Security.AccessControl;
+using AvaloniaApplication1.Comms;
 using AvaloniaApplication1.Models;
 
 namespace AvaloniaApplication1.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    
     private Display _display;
     private Comms.NetClient _client;
-
+    AsynchronousClient tcp;
+    
     /// <summary>
     /// Constructor
     /// </summary>
     public MainWindowViewModel()
     {
         _display = new Display();
-        _client = new Comms.NetClient();
-
-        
     }
 
     public void Connect()
     {
-        _client.Connect("glasstty.com", 6502);
+        //_client = new Comms.NetClient("glasstty.com", 6502);
+        try
+        {
+            tcp = new AsynchronousClient("46.101.66.218", int.Parse("6502"));
+            tcp.OnConnectEvent += new AsynchronousClient.OnConnectEventHandler(OnConnect);
+            tcp.OnDataRecievedEvent += new AsynchronousClient.DataReceivedEventHandler(OnRecieved);
+            tcp.Connect();
+        }
+        catch (Exception ex)
+        {
+            // Catch errors in Connection and Recieve Callbacks
+            Debug.Print("Error : " + ex.ToString());
+        }
     }
-    public void Disconnect()
+
+    // Connection Status Listner
+    private void OnConnect(bool status)
     {
-        _client.Disconnect();
+        Debug.Print("Connection : " + status.ToString());
+    }
+
+    // Data Recieved Listner
+    private void OnRecieved(string data)
+    {
+        Debug.Print(data);
     }
     
+    public void Disconnect()
+    {
+       if (tcp.Write("*"))
+       {
+           Debug.Print("Data Sent");
+       }
+       
+       
+    }
 
     public Display DisplayData
     {
@@ -40,10 +70,7 @@ public partial class MainWindowViewModel : ViewModelBase
             _display = value;
             OnPropertyChanged(nameof(DisplayData));
         }
-        get
-        {
-            return _display;
-        }
+        get { return _display; }
     }
 
     // implmentation of INotify for properties
