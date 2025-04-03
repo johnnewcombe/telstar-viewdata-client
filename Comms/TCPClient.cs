8d945c1d44b30457858acd8cd1c9b57efefce80a
John Newcombe (GlassTTY) <jnewcombeuk@gmail.com>
2025-04-03 23:08:06 +0100

    Updated main window layout.

--- Comms/TCPClient.cs
+++ Comms/TCPClient.cs
@@ -2,21 +2,22 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+
 /*
    MIT License
-   
+
    Copyright (c) 2019 Softink Lab
-   
+
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
    in the Software without restriction, including without limitation the rights
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
    copies of the Software, and to permit persons to whom the Software is
    furnished to do so, subject to the following conditions:
-   
+
    The above copyright notice and this permission notice shall be included in all
    copies or substantial portions of the Software.
-   
+
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,21 +25,22 @@ using System.Text;
    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
    SOFTWARE.
-   
+
  */
 
+namespace TelstarClient.Comms {
 
-namespace TelstarClient.Comms
-{
-    public class TCPClient
-    {
+    public class TCPClient {
         // *** Event Handlers *** //
 
+        #region Delegates
+        
         /// <summary>
         /// Notify the Received Data
         /// </summary>
         /// <param name="data">Received Data</param>
         public delegate void DataReceivedEventHandler(string data);
+
         public event DataReceivedEventHandler OnDataReceivedEvent;
 
         /// <summary>
@@ -46,28 +48,32 @@ namespace TelstarClient.Comms
         /// </summary>
         /// <param name="status">Connection Status</param>
         public delegate void OnConnectEventHandler(bool status);
+
         public event OnConnectEventHandler OnConnectEvent;
 
-        // *** Properties *** //
+        #endregion
+        
+        #region Properties
 
         // Connection Parameters
         private IPAddress ipAddress;
         private int port;
-        
-        
+
+
         // Socket Parameters
         private Socket socket;
-        private byte[] readerBuffer = new byte[256];
+        private byte[] readerBuffer = new byte[1024];
 
-        // *** Methods *** //
+        #endregion
+
+        #region Public Methods
 
         /// <summary>
         /// Create a TCP asynchronous client. This client is connect to the server and port with passed parameters.
         /// </summary>
         /// <param name="_ip">Server IP</param>
         /// <param name="_port">Server Port</param>
-        public TCPClient(string _ip, int _port)
-        {
+        public TCPClient(string _ip, int _port) {
             //ipAddress = IPAddress.Parse(_ip);
             ipAddress = (Dns.Resolve(_ip)).AddressList[0];
             port = _port;
@@ -76,13 +82,10 @@ namespace TelstarClient.Comms
         /// <summary>
         /// Connect to the server
         /// </summary>
-        public void Connect()
-        {
-            try
-            {
+        public void Connect() {
+            try {
                 // Close the socket if open
-                if (socket != null && socket.Connected)
-                {
+                if (socket != null && socket.Connected) {
                     socket.Shutdown(SocketShutdown.Both);
                     System.Threading.Thread.Sleep(10);
                     socket.Close();
@@ -99,8 +102,7 @@ namespace TelstarClient.Comms
                 AsyncCallback onconnect = new AsyncCallback(OnConnect);
                 socket.BeginConnect(epServer, onconnect, socket);
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 OnConnectEvent(false);
                 throw new Exception($"Socket Connection Failed. Message : {ex}");
             }
@@ -110,90 +112,12 @@ namespace TelstarClient.Comms
         /// Check connection status of the socket
         /// </summary>
         /// <returns>True or False based on status</returns>
-        public bool IsConnected()
-        {
-            if (socket != null)
-            {
+        public bool IsConnected() {
+            if (socket != null) {
                 return socket.Connected;
             }
+
             return false;
-        }
-
-        // Setup Callbacks if Socket is Connected
-        private void OnConnect(IAsyncResult ar)
-        {
-            Socket _socket = (Socket)ar.AsyncState;
-
-            try
-            {
-                if (_socket.Connected)
-                {
-                    SetupRecieveCallback(_socket);
-                    OnConnectEvent(true);
-                }
-                else
-                {
-                    OnConnectEvent(false);
-                    throw new Exception("Cannot Establish the Socket Connection");
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        // Setup Recieve Callback for Async Listning
-        private void SetupRecieveCallback(Socket _socket)
-        {
-            try
-            {
-                AsyncCallback receiveData = new AsyncCallback(OnReceivedData);
-                _socket.BeginReceive(readerBuffer, 0, readerBuffer.Length, SocketFlags.None, receiveData, _socket);
-            }
-            catch (Exception ex)
-            {
-                Dispose();
-                throw new Exception($"Receive Callback Setup Failed: {ex}");
-            }
-        }
-
-        // Recieve data from TCP
-        private void OnReceivedData(IAsyncResult ar)
-        {
-            Socket _socket = (Socket)ar.AsyncState;
-
-            if (IsConnected())
-            {
-                try
-                {
-                    // Check data is available
-                    int nBytesRec = _socket.EndReceive(ar);
-                    if (nBytesRec > 0)
-                    {
-                        string sRecieved = "";
-                        for (int i = 0; i < nBytesRec; i++)
-                        {
-                            sRecieved += (char)readerBuffer[i];
-                        }
-
-                        // Fire Data Recieved Event
-                        OnDataReceivedEvent(sRecieved);
-
-                        // If the Connection is Still Usable Re-establish the Callback
-                        SetupRecieveCallback(_socket);
-                    }
-                    else
-                    {
-                        Dispose();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Dispose();
-                    throw new Exception($"Receive Operation Failed: {ex}");
-                }
-            }
         }
 
         /// <summary>
@@ -201,25 +125,20 @@ namespace TelstarClient.Comms
         /// </summary>
         /// <param name="_data">Data to be written</param>
         /// <returns>Success status as Boolean Value</returns>
-        public bool Write(String _data)
-        {
+        public bool Write(String _data) {
             // Check Connection
-            if (IsConnected())
-            {
-                try
-                {
+            if (IsConnected()) {
+                try {
                     Byte[] byteDateLine = Encoding.ASCII.GetBytes(_data.ToCharArray());
                     socket.Send(byteDateLine, byteDateLine.Length, 0);
                     return true;
                 }
-                catch (Exception ex)
-                {
+                catch (Exception ex) {
                     Dispose();
                     throw new Exception($"Data Writing Operation Failed: {ex}");
                 }
             }
-            else
-            {
+            else {
                 return false;
             }
         }
@@ -229,24 +148,19 @@ namespace TelstarClient.Comms
         /// </summary>
         /// <param name="_data">Data to be written</param>
         /// <returns>Success status as Boolean Value</returns>
-        public bool Write(byte[] _data)
-        {
+        public bool Write(byte[] _data) {
             // Check Connection
-            if (IsConnected())
-            {
-                try
-                {
+            if (IsConnected()) {
+                try {
                     socket.Send(_data, _data.Length, 0);
                     return true;
                 }
-                catch (Exception ex)
-                {
+                catch (Exception ex) {
                     Dispose();
                     throw new Exception("Data Writing Operation Failed.");
                 }
             }
-            else
-            {
+            else {
                 return false;
             }
         }
@@ -254,10 +168,8 @@ namespace TelstarClient.Comms
         /// <summary>
         /// Close the Socket Connection
         /// </summary>
-        public void Dispose()
-        {
-            if (socket != null && socket.Connected)
-            {
+        public void Dispose() {
+            if (socket != null && socket.Connected) {
                 OnConnectEvent(false);
                 socket.Shutdown(SocketShutdown.Both);
                 socket.Close();
@@ -267,14 +179,81 @@ namespace TelstarClient.Comms
         /// <summary>
         /// Disconnect the socket
         /// </summary>
-        public void Disconnect()
-        {
-            if (socket != null && socket.Connected)
-            {
+        public void Disconnect() {
+            if (socket != null && socket.Connected) {
                 OnConnectEvent(false);
                 socket.Shutdown(SocketShutdown.Both);
                 socket.Close();
             }
         }
+
+        #endregion
+
+        #region Private Methods
+
+        // Setup Callbacks if Socket is Connected
+        private void OnConnect(IAsyncResult ar) {
+            Socket _socket = (Socket)ar.AsyncState;
+
+            try {
+                if (_socket.Connected) {
+                    SetupRecieveCallback(_socket);
+                    OnConnectEvent(true);
+                }
+                else {
+                    OnConnectEvent(false);
+                    throw new Exception("Cannot Establish the Socket Connection");
+                }
+            }
+            catch (Exception ex) {
+                throw ex;
+            }
+        }
+
+        // Setup Recieve Callback for Async Listning
+        private void SetupRecieveCallback(Socket _socket) {
+            try {
+                AsyncCallback receiveData = new AsyncCallback(OnReceivedData);
+                _socket.BeginReceive(readerBuffer, 0, readerBuffer.Length, SocketFlags.None, receiveData, _socket);
+            }
+            catch (Exception ex) {
+                Dispose();
+                throw new Exception($"Receive Callback Setup Failed: {ex}");
+            }
+        }
+
+        // Recieve data from TCP
+        private void OnReceivedData(IAsyncResult ar) {
+            Socket _socket = (Socket)ar.AsyncState;
+
+            if (IsConnected()) {
+                try {
+                    // Check data is available
+                    int nBytesRec = _socket.EndReceive(ar);
+                    if (nBytesRec > 0) {
+                        string sRecieved = "";
+                        for (int i = 0; i < nBytesRec; i++) {
+                            sRecieved += (char)readerBuffer[i];
+                        }
+
+                        // Fire Data Recieved Event
+                        OnDataReceivedEvent(sRecieved);
+
+                        // If the Connection is Still Usable Re-establish the Callback
+                        SetupRecieveCallback(_socket);
+                    }
+                    else {
+                        Dispose();
+                    }
+                }
+                catch (Exception ex) {
+                    Dispose();
+                    throw new Exception($"Receive Operation Failed: {ex}");
+                }
+            }
+        }
+
+        #endregion
+
     }
 }
