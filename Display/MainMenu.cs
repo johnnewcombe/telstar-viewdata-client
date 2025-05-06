88ca49c5cdb3bda1f122dda29e717a8c83de1290
John Newcombe (GlassTTY) <jnewcombeuk@gmail.com>
2025-05-06 22:47:03 +0100

    Updated the logo.

--- Display/MainMenu.cs
+++ Display/MainMenu.cs
@@ -30,26 +30,24 @@ public static class MainMenu {
 	/// </summary>
 	/// <returns></returns>
 	public static string GetLogo() {
-		
+		// https://edit.tf/#0:QIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQICxpAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgLGkCBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECAsaQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQICxpBw4cEHBAgQYOHBgg4cOCDBw4MCvDhw4YOHDgw4IEHBAgLGtTdGrS_0CBB_Ro_-pujRpf6NGrKo9bdGiR_0aH-gQf0CAsa1NVW9r_QIEH9ev_ol6_e0Vr1_0qg1NUCBB_QIES9f_QICxpV9-fkv_58af0CD-q-fPyXb8-PyqDU1QIEH9Ag2_Pj9AgLGkCBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECAsaQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQICxpAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgLGkCBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECAsaQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQICxpAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgLGkCBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECAsaQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQICxpAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgLGkCBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECAsaQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQICxpAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgLGkCBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECAsaQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECBAgQIECA
 		var logo = new StringBuilder();
-		logo.Append("\r\n\n\n\n\n\n");
-		logo.Append(Converters.ConvertFromMarkup("                   [G]T\r\n"));
-		logo.Append(Converters.ConvertFromMarkup("                 [G]T[R]E[C]L\r\n"));
-		logo.Append(Converters.ConvertFromMarkup("               [G]T[R]E[C]L[B]S[W]T\r\n"));
-		logo.Append(Converters.ConvertFromMarkup("             [G]T[R]E[C]L[B]S[W]T[M]A[Y]R\r\n"));
-		logo.Append(Converters.ConvertFromMarkup("               [C]L[B]S[W]T[M]A[Y]R\r\n"));
-		logo.Append(Converters.ConvertFromMarkup("                 [W]T[M]A[Y]R\r\n"));
-		logo.Append(Converters.ConvertFromMarkup("                   [Y]R\r\n"));
-
-		//logo.Append(Converters.ConvertFromMarkup("0123456789012345678901234567890123456789"));
 		
+		// a quirky but efficient way to display the GlassTTY logo
+		logo.Append("\r\n\n\n\n\n\n");
+		logo.Append(Converters.ConvertFromMarkup("[w]///////////////////////////////////////\r\n\n"));
+		logo.Append(Converters.ConvertFromMarkup("[c][sg] ppp p   `pp0 ppp `pp0 pppp`ppp0p  p\r\n"));
+		logo.Append(Converters.ConvertFromMarkup("[c][sg]j7#+%\x7f   \x7f##\x7fj7##%\x7f##+ #k7#\"#\x7f#!\x7f  \x7f\r\n"));
+		logo.Append(Converters.ConvertFromMarkup("[c][sg]j5*o5\x7f   \x7f//\x7f\"//o4+//}  j5   \x7f  \"//\x7f\r\n"));
+		logo.Append(Converters.ConvertFromMarkup("[c][sg]*}|~%\x7f||4\x7f  \x7f*||~%m||?  j5   \x7f  m||?\r\n"));
+		logo.Append(Converters.ConvertFromMarkup("\r\n\n\n"));
+		logo.Append(Converters.ConvertFromMarkup("[D][n][B]         Viewdata Terminal\r\n"));
+		
+		// alignment guide
+		//logo.Append(Converters.ConvertFromMarkup("0123456789012345678901234567890123456789"));
 		
 		return logo.ToString();
 		
-		// TODO use markup?? or edit.tf format.
-		// i.e. define a string and convert it to bytes
-		//var row = new List<byte>(){esc,G,(byte)'T'};
-
 	}
 
 	public static string GetMenu() {
