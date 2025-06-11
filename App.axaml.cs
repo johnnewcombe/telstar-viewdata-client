141aa44af12f4c374cf274aa6c9430b299f6c506
John Newcombe (GlassTTY) <jnewcombeuk@gmail.com>
2025-06-11 21:40:40 +0100

    Fixed issue where MainWindowViewController was being instantiated twice.

--- App.axaml.cs
+++ App.axaml.cs
@@ -47,7 +47,7 @@ public partial class App : Application
             DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                //DataContext = new MainWindowViewModel(),
             };
         }
         
